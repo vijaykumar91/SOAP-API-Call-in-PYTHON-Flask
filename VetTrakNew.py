725f3f500cf41d6abdf0990caedfcdf3bc4b4c1e
vijaykumar91 <35297830+vijaykumar91@users.noreply.github.com>
2019-07-24 15:51:19 +0530

    Update VetTrakNew.py

--- VetTrakNew.py
+++ VetTrakNew.py
@@ -18,7 +18,7 @@ server = 'dbserveranasight.database.windows.net'
 database = 'warehouse'
 username = 'anasight@dbserveranasight'
 password = 'Root@12345'
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
 
